ab88bd2b5b22eb87e35be5d9f12b8c1d2ee07184
kabornblack <shaibu.kabiru@email.com>
2023-08-01 21:54:08 +0300

    Connected project project wikiDB to mongoDB through Studio 3T and create an articles collection

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
@@ -10,10 +10,38 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
+
+const dbURI = 'mongodb://127.0.0.1:27017/wikiDB';
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+    unique: true,
+  }
+});
+
+const Article = mongoose.model('Article', articleSchema);
 
 //TODO
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(8000, function() {
+  console.log("Server started on port 8000");
 });
